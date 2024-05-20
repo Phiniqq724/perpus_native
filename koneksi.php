4fa7024fb29ecd4b0e55b764d2b73c334a3094dd
phiniqq724 <fahrellnew@gmail.com>
2024-05-20 16:42:13 +0700

    Koneksi php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$conn=mysqli_connect('localhost','root','','perpus_native');
+/* check connection */
+if (mysqli_connect_error()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    echo "gagal";
+    exit();
+}
+?>
